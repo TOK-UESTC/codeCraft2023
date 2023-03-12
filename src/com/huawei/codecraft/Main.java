476bf7f93380e894aa196442b8c824fc452f710e
rocinate <rocinate1999@gmail.com>
2023-03-12 22:10:30 +0800

    简化了main函数中的计时器

--- src/com/huawei/codecraft/Main.java
+++ src/com/huawei/codecraft/Main.java
@@ -3,23 +3,25 @@ package com.huawei.codecraft;
 import java.io.*;
 import java.util.*;
 
+import com.huawei.codecraft.utils.MyTimer;
+
 public class Main {
 
     private static final Scanner inStream = new Scanner(System.in);
 
     private static final PrintStream outStream = new PrintStream(new BufferedOutputStream(System.out));
 
+    // 时间开关
     private static final boolean showTime = false;
+    // 日志开关
     private static final boolean saveLog = false;
 
+    private static final int totalFrame = 50 * 60 * 3;
     private static final Context ctx = new Context(inStream, outStream, saveLog);
+    private static final MyTimer timer = new MyTimer(totalFrame);
 
     public static void main(String[] args) {
         ctx.init();
-
-        int totalFrame = 50 * 60 * 3;
-        long startTime = System.nanoTime();
-        long lastTime = System.nanoTime();
 
         // 经验证，答题器并不会输出第0帧的信息，故可以忽略控制台输出的
         // player skipped frames: 0
@@ -29,17 +31,10 @@ public class Main {
             ctx.step();
 
             if (showTime) {
-                long endTime = System.nanoTime();
-                System.err.printf("time: [%.3f]ms\n", (endTime - lastTime) / 1000000.0);
-                System.err.flush();
-                lastTime = endTime;
+                timer.showTime();
             }
         }
 
-        double totalTime = (System.nanoTime() - startTime) / 1000000.0;
-        double avgTime = totalTime / totalFrame;
-        System.err.printf("* total: [%.3f]ms\n", totalTime);
-        System.err.printf("* avg: [%.3f]ms\n", avgTime);
-        System.err.flush();
+        timer.showStatic();
     }
 }
